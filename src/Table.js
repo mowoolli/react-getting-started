c2f118a8083376badf086dac906dab84e560d0c6
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-22 09:53:25 -0700

    Add state to the app; delete table data feature

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const TableHeader = () => {
   return (
@@ -8,7 +8,8 @@ const TableHeader = () => {
 
 const TableBody = props => {
   const rows = props.characterData.map((row, index) => {
-    return (<tr key={index}><td>{row.name}</td><td>{row.job}</td></tr>)
+    return (<tr key={index}><td>{row.name}</td><td>{row.job}</td>
+      <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td></tr>)
   })
 
   return (
@@ -16,14 +17,13 @@ const TableBody = props => {
   )
 }
 
-class Table extends Component {
-  render() {
-    const { characterData } = this.props
+const Table = props => {
+  const { characterData, removeCharacter } = props
 
-    return (
-      <table><TableHeader /><TableBody characterData={characterData} /></table>
-    )
-  }
+  return (
+    <table><TableHeader /><TableBody characterData={characterData} removeCharacter={removeCharacter} /></table>
+  )
+
 }
 
 export default Table
