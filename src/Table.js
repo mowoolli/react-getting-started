7f8ea0cb803cff9ab74659cf6aa376033f9504c5
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-22 09:12:25 -0700

    Construct the table with simple components

--- src/Table.js
+++ src/Table.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
+const TableHeader = () => {
+  return (
+    <thead><tr><th>Name</th><th>Job</th></tr></thead>
+  )
+}
+
+const TableBody = () => {
+  return (
+    <tbody><tr><td>Charlie</td><td>Janitor</td></tr>
+      <tr><td>Mac</td><td>Bouncer</td></tr>
+      <tr><td>Dee</td><td>Aspiring Actress</td></tr>
+      <tr><td>Dennis</td><td>Bartender</td></tr></tbody>
+  )
+}
+
 class Table extends Component {
   render() {
     return (
-      <table><thead><tr><th>Name</th><th>Job</th></tr></thead>
-        <tbody><tr><td>Charlie</td><td>Janitor</td></tr>
-          <tr><td>Mac</td><td>Bouncer</td></tr>
-          <tr><td>Dee</td><td>Aspiring Actress</td></tr>
-          <tr><td>Dennis</td><td>Bartender</td></tr></tbody></table>
+      <table><TableHeader /><TableBody /></table>
     )
   }
 }
